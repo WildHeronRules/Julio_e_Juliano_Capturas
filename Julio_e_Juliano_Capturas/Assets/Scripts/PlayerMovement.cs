75ce58dbf9d89a88451604e6c8b66900bb75489a
WildHeronRules <47932182+WildHeronRules@users.noreply.github.com>
2021-05-09 16:55:10 -0300

    Movimentos Básicos

---
+++ Julio_e_Juliano_Capturas/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed = 1f;
+    public Rigidbody2D rb;
+    Vector2 move;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey("left shift"))
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 3f;
+        }
+
+        if(move.x != 0 && move.y != 0){
+            speed = speed - 0.5f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.deltaTime);
+
+    }
+
+
+
+
+}
