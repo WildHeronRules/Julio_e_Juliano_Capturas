1b254937f03384905fb8971d91a7735d4379e851
KenseLiz <79674493+KenseLiz@users.noreply.github.com>
2021-06-04 18:03:59 -0300

    Add files via upload

---
+++ CausarDano.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CausarDano : MonoBehaviour
+{
+
+    public Inimigo enemy;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.name == "Inimigo"){
+            enemy.HP += (-10);
+            Debug.Log("Bateu");
+        }
+
+    }
+}
