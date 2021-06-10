1007ac929aec78c5fc1a29088d9694461fd2d259
XXipe <gixipe@gmail.com>
2021-06-10 04:24:53 -0300

    Banco de dados adicionado

---
+++ Julio_e_Juliano_Capturas/Assets/Scripts/BancoDeDados/SaveScore.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class SaveScore : MonoBehaviour
+{
+    public Text pontuacao;
+    public Text status;
+    public InputField nome;
+    public Button salvar;
+
+    public int pontos;
+    public string jogador;
+
+    void Start()
+    {
+        //pontuacao.text = "Você fez " + PlayerPrefs.GetInt("Pontuacao") + " pontos";
+        //pontos = PlayerPrefs.GetInt("Pontuacao");
+    }
+
+    void Update()
+    {
+        jogador = nome.text;
+        //pontos = PlayerPrefs.GetInt("Pontuacao");
+    }
+
+    public void SalvarBtn()
+    {
+        if (nome.text != "")
+        {
+            StartCoroutine(AdicionarPontuacao());
+            status.text = "Registrando pontuacao";
+            salvar.enabled = false;
+            nome.enabled = false;
+            //PlayerPrefs.SetInt("Pontuacao", 0);
+        }
+        else
+        {
+            status.text = "Escreva um nome primeiro";
+        }
+    }
+
+    IEnumerator AdicionarPontuacao()
+    {
+        WWWForm wwwf = new WWWForm();
+        wwwf.AddField("jogador", jogador);
+        wwwf.AddField("pontos", pontos);
+
+        UnityWebRequest w = UnityWebRequest.Post("http://localhost/jejc_bd/inserirPontuacao.php", wwwf);
+
+        yield return w.SendWebRequest();
+
+        if (w.isHttpError || w.isNetworkError)
+        {
+            Debug.Log("Erro: " + w.error);
+        }
+        else
+        {
+            if (w.downloadHandler.text.Equals("Ok"))
+            {
+                status.text = "Sua pontuacao foi salva";
+            }
+        }
+    }
+}
---
+++ Julio_e_Juliano_Capturas/Assets/Scripts/HighScores.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HighScores : MonoBehaviour
+{
+
+    public Text[] NomesJogadores;
+    public Text[] PontosJogadores;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(listarJogadores());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AtualizarLista()
+    {
+
+        StartCoroutine(listarJogadores());
+    }
+
+    IEnumerator listarJogadores()
+    {
+        WWWForm wwwf = new WWWForm();
+        wwwf.AddField("limite", 5);
+
+        UnityWebRequest w = UnityWebRequest.Post("http://localhost/jejc_bd/listarJogadores.php", wwwf);
+
+        yield return w.SendWebRequest();
+
+        if (w.isHttpError || w.isNetworkError)
+        {
+            Debug.Log("Erro: " + w.error);
+        }
+        else
+        {
+            Pontuacao pontuacaoContainer = JsonUtility.FromJson<Pontuacao>(w.downloadHandler.text);
+
+            for (int i = 0; i < pontuacaoContainer.objetos.Length; i++)
+            {
+                NomesJogadores[i].text = pontuacaoContainer.objetos[i].jogador;
+                PontosJogadores[i].text = pontuacaoContainer.objetos[i].pontos.ToString();
+            }
+        }
+    }
+}
